0dda6a08fd05d23f029c736dfdefc48b6a5e49f3
Elvis Bui <elvisbui.ed@gmail.com>
2019-11-27 11:33:03 -0500

    Completed Example of Scorekeeper Project

--- Udemy/The Web Developer Bootcamp/Section 16 - Advanced Dom Manipulation/Score Keeper Project/scorekeeper.js
+++ Udemy/The Web Developer Bootcamp/Section 16 - Advanced Dom Manipulation/Score Keeper Project/scorekeeper.js
@@ -3,10 +3,13 @@ var p2Button = document.getElementById("p2");
 var resetButton = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
 var winningScore = 5;
+var winningScoreDisplay = document.querySelector("p span");
+
 
 p1Button.addEventListener("click", function(){
     if(!gameOver){
@@ -28,10 +31,12 @@ p2Button.addEventListener("click", function(){
         }
         p2Display.textContent = p2Score;
     }
-
 })
 
 resetButton.addEventListener("click", function(){
+    reset();
+})
+function reset(){
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = p1Score;
@@ -39,4 +44,9 @@ resetButton.addEventListener("click", function(){
     p1Display.classList.remove("winner");
     p2Display.classList.remove("winner");
     gameOver = false;
+}
+numInput.addEventListener("change",function(){
+    winningScoreDisplay.textContent = numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
 })
