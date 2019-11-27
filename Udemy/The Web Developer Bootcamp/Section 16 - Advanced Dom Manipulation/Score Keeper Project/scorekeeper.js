136ed553c26969afa5fac5346d9d4dd9893fc633
Elvis Bui <elvisbui.ed@gmail.com>
2019-11-27 11:16:31 -0500

    Initial Version of Scorekeeper Project

--- Udemy/The Web Developer Bootcamp/Section 16 - Advanced Dom Manipulation/Score Keeper Project/scorekeeper.js
+++ Udemy/The Web Developer Bootcamp/Section 16 - Advanced Dom Manipulation/Score Keeper Project/scorekeeper.js
@@ -13,7 +13,7 @@ p1Button.addEventListener("click", function(){
         p1Score++;
         if(p1Score === winningScore){
             gameOver = true;
-            p1Display.style.color = "green";
+            p1Display.classList.add("winner");
         }
         p1Display.textContent = p1Score;
     }
@@ -24,7 +24,7 @@ p2Button.addEventListener("click", function(){
         p2Score++;
         if(p2Score === winningScore){
             gameOver = true;
-            p2Display.style.color = "green";
+            p2Display.classList.add("winner");
         }
         p2Display.textContent = p2Score;
     }
@@ -36,4 +36,7 @@ resetButton.addEventListener("click", function(){
     p2Score = 0;
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+    gameOver = false;
 })
